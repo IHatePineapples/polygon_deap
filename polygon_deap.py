6a97d3fb9c51f9879df6b549d698be1e779a5007
Small Pineapple <lostinhollywood@duck.com>
2022-05-07 16:28:48 +0100

    Change population size

--- polygon_deap.py
+++ polygon_deap.py
@@ -100,7 +100,7 @@ def main():
     toolbox.register("mutate", mutate, indpb=0.5)
     toolbox.register("mate", tools.cxTwoPoint)
 
-    population = toolbox.population(n=40)
+    population = toolbox.population(n=100)
 
 
     hof = tools.HallOfFame(3)
